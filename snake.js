979434207aad7a5afb2c5d24e018b71978a0f881
prateeksawhney97 <prateek.sawhney97@gmail.com>
2020-04-03 14:26:14 +0530

    snake done

--- snake.js
+++ snake.js
@@ -2,28 +2,32 @@ function init(){
 	canvas = document.getElementById('mycanvas');
 	W = H = canvas.width = canvas.height = 1000;
 	pen = canvas.getContext('2d');
-	cs:50;
+	cs=66;
 
 	snake = {
-		init_len=5,
+		init_len:5,
 		color: "blue",
 		cells: [],
 		direction: "right",
 
 		createSnake:function(){
-			for (var i = this.init_len - 1; i >= 0; i--) {
+			for (var i = this.init_len; i >0; i--) {
 				this.cells.push({x:i,y:0});
 			}
 		},
 
 		drawSnake:function(){
-			pen.fillRect(this.cells[i].x,this.cells[i].y,cs,cs);
+			for(var i=0;i<this.cells.length;i++){
+				pen.fillRect(this.cells[i].x*cs,this.cells[i].y*cs,cs-2,cs-2);	
+			}
 		},
 	};
+
+	snake.createSnake();
 }
 
 function draw(){
-
+	snake.drawSnake();
 
 }
 
@@ -34,7 +38,8 @@ function update(){
 
 
 function gameloop(){
-
+	draw();
+	update();
 }
 
 init();
