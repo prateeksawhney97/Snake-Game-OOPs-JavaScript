1425e253f3dadf9a74914e498ff663bfdcd41768
prateeksawhney97 <prateek.sawhney97@gmail.com>
2020-04-03 14:15:37 +0530

    creating snake

--- snake.js
+++ snake.js
@@ -1,5 +1,25 @@
 function init(){
+	canvas = document.getElementById('mycanvas');
+	W = H = canvas.width = canvas.height = 1000;
+	pen = canvas.getContext('2d');
+	cs:50;
 
+	snake = {
+		init_len=5,
+		color: "blue",
+		cells: [],
+		direction: "right",
+
+		createSnake:function(){
+			for (var i = this.init_len - 1; i >= 0; i--) {
+				this.cells.push({x:i,y:0});
+			}
+		},
+
+		drawSnake:function(){
+			pen.fillRect(this.cells[i].x,this.cells[i].y,cs,cs);
+		},
+	};
 }
 
 function draw(){
